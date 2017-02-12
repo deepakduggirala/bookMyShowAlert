2c046cf1332938a4c41f76e4634ea26a257f69d1
Deepak Duggirala <deepakduggirala.dd@gmail.com>
2017-02-12 20:59:11 +0530

    add user script code

--- alertService.js
+++ alertService.js
@@ -1,34 +1,48 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://in.bookmyshow.com/bengaluru/movies
+// @grant        none
+// @require http://code.jquery.com/jquery-3.1.1.min.js
+// ==/UserScript==
+
 (function(query, interval) {
     'use strict';
     function check(callback){
         var movies = $('#now-showing > section.now-showing.filter-now-showing > div > div.__col-now-showing > div.mv-row > div > div > div > div.detail > div.__name.overflowEllipses > a')
-                      .map(()=> $(this).text());
+        .map(()=> $(this).text());
         if(movies.length === 0)
             callback();
         callback(Array.from(movies).filter((name)=>name.toLowerCase().indexOf(query.toLowerCase()) !== -1).length !== 0);
     }
-	
-	var repeat = setInterval(function(){
-		check(function(found){
-			if(typeof found === 'undefined'){
-				customAlert('Error');
-				stopRepeat();
-			}
-			else if(typeof found === 'boolean')
-				customAlert(found);
-				if(!found)
-					location.reload();
-		});
-	}, 1000*interval);
-	
-	function stopRepeat(){
-		clearInterval(repeat);
-	}
-	
-	function customAlert(found){
-		if(found)
-			alert("Now available: "+query);
-		else
-			console.log('Not available yet');
-	}
-})('wick', 10);
+
+    var repeat = setInterval(function(){
+        check(function(found){
+            if(typeof found === 'undefined'){
+                customAlert('Error');
+                stopRepeat();
+            }
+            else if(typeof found === 'boolean'){
+                customAlert(found);
+                if(!found)
+                    location.reload();
+            }
+        });
+    }, 1000*interval);
+
+    function stopRepeat(){
+        clearInterval(repeat);
+    }
+
+    function customAlert(found){
+        if(found){
+            alert("Now available: "+query);
+            stopRepeat();
+        }
+        else
+            console.log('Not available yet');
+    }
+})('wick', 120);
