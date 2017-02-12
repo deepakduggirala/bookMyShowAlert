6b4d06a8144154b25428f58c61f8985ca7a67530
Deepak Duggirala <deepakduggirala.dd@gmail.com>
2017-02-12 22:35:46 +0530

    remove setInterval, add notifications

--- alertService.js
+++ alertService.js
@@ -11,45 +11,53 @@
 
 (function(query, interval) {
     'use strict';
-	var checkRunner = function(){
-        check(function(found){
-            console.log(found);
-            if(typeof found === 'undefined'){
-                customAlert('Error');
-                stopRepeat();
-            }
-            else if(typeof found === 'boolean'){
-                customAlert(found);
-                if(!found)
-                    location.reload();
-            }
-        });
-    };
-	
     function check(callback){
         var movies = Array.from($('#now-showing > section.now-showing.filter-now-showing > div > div.__col-now-showing > div.mv-row > div > div > div > div.detail > div.__name.overflowEllipses > a')
         .map(function(){
             return $(this).text();
         }));
         if(movies.length === 0)
-            callback();
-        callback(movies.filter((name)=>name.toLowerCase().indexOf(query.toLowerCase()) !== -1).length !== 0);
+            callback({
+				statusCode:500,
+				msg : 'Error: No movies found'
+			});
+		else if(movies.filter((name)=>name.toLowerCase().indexOf(query.toLowerCase()) !== -1).length !== 0)
+			callback({
+				statusCode : 200,
+				msg : query + ' found'
+			});
+		else
+			callback({
+				statusCode : 404,
+				msg : query + ' not found'
+			});
     }
-
-    var repeat = setInterval(checkRunner, 1000*interval);
-
-    function stopRepeat(){
-        clearInterval(repeat);
-    }
-
-    function customAlert(found){
-        if(found){
-            alert("Now available: "+query);
-            stopRepeat();
-        }
-        else
-            console.log('Not available yet');
-    }
-	
-	checkRunner();
+	function notify(message){
+		// Let's check whether notification permissions have already been granted
+		console.log(message);
+		if (Notification.permission === "granted") {
+			// If it's okay let's create a notification
+			var notification = new Notification(message);
+		}
+	}
+	(function setUpNotify() {
+		// Let's check if the browser supports notifications
+	    if (!("Notification" in window)) {
+			console.log("This browser does not support desktop notification");
+	    }
+		// Otherwise, we need to ask the user for permission
+		else if (Notification.permission !== 'denied') {
+			Notification.requestPermission();
+		}
+	})();
+	(function checkRunner(){
+        check(function(res){
+            if(res.statusCode === 404)
+				setTimeout(function(){
+					location.reload();
+				}, 1000*interval);
+			else
+				notify(res.msg);
+        });
+    })();
 })('wick', 10*60);
